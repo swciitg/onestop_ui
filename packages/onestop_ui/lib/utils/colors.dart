0fc6a6d963c775535565a4f6bb7071984bf7ec3b
Venkatesh M <venkylm10@gmail.com>
2025-05-30 00:57:37 +0530

    base colors

---
+++ packages/onestop_ui/lib/utils/colors.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class OColors {
+  static const primary = OBaseColors.green600;
+}
+
+class OBaseColors {
+  // Green colors
+  static const green100 = Color(0xFFAEF2C4);
+  static const green200 = Color(0xFF7AEBA7);
+  static const green300 = Color(0xFF52D09B);
+  static const green400 = Color(0xFF27D06F);
+  static const green500 = Color(0xFF14BD56);
+  static const green600 = Color(0xFF146541);
+  static const green700 = Color(0xFF0B5974);
+  static const green800 = Color(0xFF00331A);
+
+  // Blue colors (Accent)
+  static const blue100 = Color(0xFFEDDFFF);
+  static const blue200 = Color(0xFF8AAFFF);
+  static const blue300 = Color(0xFF4C7AFF);
+  static const blue400 = Color(0xFF3C7BFF);
+  static const blue500 = Color(0xFF005FFD);
+  static const blue600 = Color(0xFF004BED);
+  static const blue700 = Color(0xFF00378A);
+  static const blue800 = Color(0xFF002237);
+
+  // Gray colors (Neutrals)
+  static const gray100 = Color(0xFFF4F3F5);
+  static const gray200 = Color(0xFFC8C8CA);
+  static const gray300 = Color(0xFF8D8D90);
+  static const gray400 = Color(0xFFC8DAEF);
+  static const gray500 = Color(0xFF95999F);
+  static const gray600 = Color(0xFF656F77);
+  static const gray700 = Color(0xFF4AC6DB);
+  static const gray800 = Color(0xFF343430);
+
+  // Semantic colors
+  // Warning (Yellow)
+  static const warning100 = Color(0xFFFEEDCF);
+  static const warning200 = Color(0xFFE6DBD0);
+  static const warning300 = Color(0xFFFAC4AD);
+  static const warning400 = Color(0xFFFFBB4C);
+  static const warning500 = Color(0xFFA4460B);
+  static const warning600 = Color(0xFFAB8D10);
+  static const warning700 = Color(0xFF6D4407);
+  static const warning800 = Color(0xFF222237);
+
+  // Negative (Red)
+  static const negative100 = Color(0xFFFACDE2);
+  static const negative200 = Color(0xFFF792A1);
+  static const negative300 = Color(0xFFF5397C);
+  static const negative400 = Color(0xFFF23544);
+  static const negative500 = Color(0xFFDC1735);
+  static const negative600 = Color(0xFFAF002A);
+  static const negative700 = Color(0xFF8D001F);
+  static const negative800 = Color(0xFF500012);
+
+  // Positive (Green - same as base green but named semantically)
+  static const positive100 = Color(0xFFAEF2C4);
+  static const positive200 = Color(0xFF7AEBA7);
+  static const positive300 = Color(0xFF52D09B);
+  static const positive400 = Color(0xFF27D06F);
+  static const positive500 = Color(0xFF14BD56);
+  static const positive600 = Color(0xFF146541);
+  static const positive700 = Color(0xFF0B5974);
+  static const positive800 = Color(0xFF00331A);
+}
