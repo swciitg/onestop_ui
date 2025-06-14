325e1541f7c4fcdba6c3885a831cc874a97bfa08
HydrogenIITG <himanshu011raj@gmail.com>
2025-06-14 18:40:13 +0530

    chore: corrected labelXSmall

---
+++ packages/onestop_ui/lib/components/ocard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OCard extends StatefulWidget {
+  const OCard({super.key});
+
+  @override
+  State<OCard> createState() => _OCardState();
+}
+
+class _OCardState extends State<OCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
