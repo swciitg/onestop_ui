b8add71287a2fd17ec24303646cb78dda2209a23
ganeshsinnur <ganeshsinnur07@gmail.com>
2025-06-19 18:05:32 +0530

    igonre commit msg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:onestop_ui/index.dart';
+import 'package:onestop_ui/widget_demo/buttons_demo.dart';
+import 'package:onestop_ui/widget_demo/list_demo.dart';
 
 void main() async {
   await GetStorage.init();
@@ -37,6 +39,18 @@ class MyApp extends StatelessWidget {
                     CardsDemo(),
                     const SizedBox(height:20),
                     TextfieldsDemo(),
+                  const SizedBox(height:10),                  
+                  const Divider(color:Color(0xFF148440),),
+                  const SizedBox(height:10),                  
+                  Text("Buttons Part",style: TextStyle(fontSize: 25),),
+                  SizedBox(height: 25,),
+                  ButtonsDemo(),
+                  const SizedBox(height:10),                  
+                  const Divider(color:Color(0xFF148440),),
+                  const SizedBox(height:10),                  
+                  Text("Buttons Part",style: TextStyle(fontSize: 25),),
+                  SizedBox(height: 25,),
+                  ListDemo()
                   ]
               ),
             ),
