00383c70534aa6db6d8814c94693ff31c42132f4
HydrogenIITG <himanshu011raj@gmail.com>
2025-06-23 14:05:17 +0530

    feat:added OProductRequestCard

--- packages/onestop_ui/lib/components/cards/buy_n_sell.dart
+++ packages/onestop_ui/lib/components/cards/buy_n_sell.dart
@@ -108,27 +108,25 @@ class _OProductListingCardState extends State<OProductListingCard> {
                     ),
                   ),
                   if (widget.isEditingEnabled == true)
-                  Padding(
-                    padding: const EdgeInsets.only(top: OSpacing.xxs),
-                    child: Divider(
-                      color: OColor.gray400,
-                      height: 2,
-                      thickness: 1,
-                      indent: OSpacing.xs,
-                      endIndent: OSpacing.xs,
+                    Padding(
+                      padding: const EdgeInsets.only(top: OSpacing.xxs),
+                      child: Divider(
+                        color: OColor.gray400,
+                        height: 2,
+                        thickness: 1,
+                        indent: OSpacing.xs,
+                        endIndent: OSpacing.xs,
+                      ),
                     ),
-                  ),
                   if (widget.isEditingEnabled == true)
-                    SizedBox(
-                      height: OSpacing.xl +12,
-                    )
+                    SizedBox(height: OSpacing.xl + 12),
                 ],
               ),
             ),
           ),
           if (widget.isEditingEnabled == true)
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal:OSpacing.xxs),
+              padding: const EdgeInsets.symmetric(horizontal: OSpacing.xxs),
               child: Row(
                 children: [
                   TextButton.icon(
@@ -185,5 +183,267 @@ class _OProductListingCardState extends State<OProductListingCard> {
   @override
   void dispose() {
     super.dispose();
+  }
+}
+
+class OProductRequestCard extends StatefulWidget {
+  final String imageURl;
+  final String itemName;
+  final String priceLabel;
+  final String price;
+  final Function()? onArrowPressed;
+  final String? userImageURl;
+  final String? userName;
+  final Function()? delete;
+  final Function()? edit;
+  final Function()? message;
+  final bool isEditingEnabled;
+  final Function()? phone;
+
+  const OProductRequestCard({
+    super.key,
+    required this.itemName,
+    this.imageURl = '',
+    required this.price,
+    required this.priceLabel,
+    this.onArrowPressed,
+    this.userImageURl,
+    this.userName,
+    this.edit,
+    this.message,
+    this.phone,
+    this.delete,
+    this.isEditingEnabled = false,
+  });
+
+  @override
+  State<OProductRequestCard> createState() => _OProductRequestCardState();
+}
+
+class _OProductRequestCardState extends State<OProductRequestCard> {
+  bool _isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(OSpacing.xs),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTapDown:
+                (_) => setState(
+                  () => _isPressed = true,
+                ), //engage behaviour when search bar is tapped
+            onTapUp: (_) {
+              setState(() => _isPressed = false);
+            },
+            child: Container(
+              padding: const EdgeInsets.all(OSpacing.xs),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(OCornerRadius.l),
+                border: Border.all(color: OColor.gray200, width: 1),
+                color: _isPressed ? OColor.gray200 : OColor.white,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(OSpacing.xs),
+                        child: Container(
+                          width: 72,
+                          height: 72,
+                          decoration: BoxDecoration(
+                            color: OColor.green100,
+                            borderRadius: BorderRadius.circular(
+                              OCornerRadius.s,
+                            ),
+                          ),
+                          child:
+                              widget.imageURl.isNotEmpty
+                                  ? ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                      OCornerRadius.s,
+                                    ),
+                                    child: Image(
+                                      image: NetworkImage(widget.imageURl),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                  : Icon(
+                                    TablerIcons.photo,
+                                    color: OColor.green600,
+                                    size: 32,
+                                  ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: OSpacing.xxs,
+                              vertical: OSpacing.xs,
+                            ),
+                            child: OText(
+                              text: widget.itemName,
+                              style: OTextStyle.labelMedium.copyWith(
+                                color: OColor.gray800,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: OSpacing.xs,
+                            ),
+                            child: OText(
+                              text: widget.priceLabel,
+                              style: OTextStyle.labelXSmall.copyWith(
+                                color: OColor.gray600,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: OSpacing.xs,
+                            ),
+                            child: OText(
+                              text: widget.price,
+                              style: OTextStyle.labelMedium.copyWith(
+                                color: OColor.gray800,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 2,
+                    color: OColor.gray200,
+                    thickness: 1,
+                    indent: OSpacing.xs,
+                    endIndent: OSpacing.xs,
+                  ),
+                  const SizedBox(height: OSpacing.xl + 5),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: OSpacing.m ),
+            child: Row(
+              mainAxisAlignment:
+                  widget.isEditingEnabled
+                      ? MainAxisAlignment.center
+                      : MainAxisAlignment.spaceBetween,
+              children: [
+                if (widget.isEditingEnabled == false)
+                  Row(
+                    children: [
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          color: OColor.gray400,
+                          image: DecorationImage(
+                            image: NetworkImage(widget.userImageURl!),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(OCornerRadius.xl),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: OSpacing.xs,
+                        ),
+                        child: OText(
+                          text: widget.userName,
+                          style: OTextStyle.labelSmall.copyWith(
+                            color: OColor.gray800,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                if (widget.isEditingEnabled == false)
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: widget.phone,
+                        icon: Icon(
+                          TablerIcons.phone,
+                          size: 24,
+                          color: OColor.green600,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: widget.message,
+                        icon: Icon(
+                          TablerIcons.message,
+                          size: 24,
+                          color: OColor.green600,
+                        ),
+                      ),
+                    ],
+                  ),
+                if (widget.isEditingEnabled == true)
+                  Row(
+                    children: [
+                      TextButton.icon(
+                        onPressed: widget.edit,
+                        icon: Icon(
+                          TablerIcons.edit,
+                          size: 16,
+                          color: OColor.green600,
+                        ),
+                        label: OText(
+                          text: "Edit",
+                          style: OTextStyle.labelSmall.copyWith(
+                            color: OColor.green600,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.25),
+                      TextButton.icon(
+                        onPressed: widget.delete,
+                        icon: Icon(
+                          TablerIcons.trash,
+                          size: 16,
+                          color: OColor.red500,
+                        ),
+                        label: OText(
+                          text: "Delete",
+                          style: OTextStyle.labelSmall.copyWith(
+                            color: OColor.red500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 6,
+            right: 6,
+            child: IconButton(
+              icon: Icon(TablerIcons.chevron_right, color: OColor.gray600),
+              onPressed: widget.onArrowPressed,
+              iconSize: 24,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
