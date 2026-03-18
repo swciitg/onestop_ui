04a2ddcd9848803fcf4fbc93981056fdd6e962a3
Venkatesh M <venkylm10@gmail.com>
2026-03-19 00:36:31 +0530

    fix: update fontFamily path in OTextStyle to include package reference

--- packages/onestop_ui/lib/utils/styles.dart
+++ packages/onestop_ui/lib/utils/styles.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class OTextStyle {
-  static const String fontFamily = 'Geist';
+  static const String fontFamily = 'packages/onestop_ui/Geist';
 
   static const displayLarge = TextStyle(
     fontFamily: fontFamily,
